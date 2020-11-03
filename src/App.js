909c4f0f09b5cc6eaeaace8ca859d1eb27926cef
vishnuk7 <vishnu.kraman88@gmail.com>
2020-11-04 02:31:46 +0530

    color seeder 📁

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
+import Palette from './Palette';
+import seedColors from './seedColors';
 
 function App() {
-	return <div className='App'>Hue</div>;
+	return (
+		<div className='App'>
+			<Palette {...seedColors[0]} />
+		</div>
+	);
 }
 
 export default App;
