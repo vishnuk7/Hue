ec01b0e3ef6e9f17fcb0cd2e9026aa681e1d770c
vishnuk7 <vishnu.kraman88@gmail.com>
2020-11-04 02:59:11 +0530

    colorbox compoent 📦

--- src/Palette.js
+++ src/Palette.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+
+import './Palette.css';
 
 export default class Palette extends Component {
 	render() {
-		return <div>Palette</div>;
+		const colorBoxes = this.props.colors.map((color) => <ColorBox background={color.color} name={color.name} />);
+
+		return (
+			<div className='Palette'>
+				<div className='Palette-colors'>{colorBoxes}</div>
+			</div>
+		);
 	}
 }
