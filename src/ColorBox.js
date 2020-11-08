55b86020825dd2f24744684eb5baccdf422d31da
vishnuk7 <vishnu.kraman88@gmail.com>
2020-11-08 23:59:11 +0530

    add dynamic text color 😃

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
 import './ColorBox.css';
 
 export default class ColorBox extends Component {
@@ -22,6 +23,10 @@ export default class ColorBox extends Component {
 	render() {
 		const { background, name, moreURL, showLink } = this.props;
 		const { copied } = this.state;
+		const isDark = chroma(background).luminance() <= 0.08;
+		const isLight = chroma(background).luminance() >= 0.65;
+
+		console.log(isDark, chroma(background).luminance());
 		return (
 			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
 				<div style={{ background }} className='ColorBox'>
@@ -32,13 +37,13 @@ export default class ColorBox extends Component {
 					</div>
 					<div className='copy-container'>
 						<div className='box-content'>
-							<span>{name}</span>
+							<span className={isDark && 'text-light'}>{name}</span>
 						</div>
-						<button className='copy-btn'>Copy</button>
+						<button className={`copy-btn ${isLight && 'text-dark'}`}>Copy</button>
 					</div>
 					{showLink && (
 						<Link to={moreURL} onClick={(e) => e.stopPropagation()}>
-							<span className='see-more'>More</span>
+							<span className={`see-more ${isLight && 'text-dark'}`}>More</span>
 						</Link>
 					)}
 				</div>
